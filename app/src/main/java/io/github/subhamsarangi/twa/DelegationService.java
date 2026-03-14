9a1d7a9009ef8fc8ad9ec07eb56cfa7a9bbfd036
subhamsarangi <subhamsarangi2016@gmail.com>
2026-03-15 01:34:49 +0530

    bubblewrap

---
+++ app/src/main/java/io/github/subhamsarangi/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.subhamsarangi.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
